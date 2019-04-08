d5948dd6bf4242d882539a62af6f583a4e14351f
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 03:28:01 +0000

    Snapshot of branch master taken on 2019-04-07T23:27:59.981-04:00 from "https://github.com/UnofficialJuliaMirror/SymmetricBitArrays.jl-8332e584-40f3-5f5a-9eee-dfdb1ad742fa"

---
+++ src/SymmetricBitArrays.jl
@@ -0,0 +1,44 @@
+module SymmetricBitArrays
+
+import Base: linearindexing, size, getindex, setindex!
+export linearindexind, size, getindex, setindex!
+export SymBitArray
+
+type SymBitArray <: AbstractArray{Bool, 2}
+    data::BitArray{1}
+    SymBitArray(n::Int) = new(BitArray{1}(Int(n*(n+1)/2)))
+end
+
+linearindexing(a::SymBitArray) = Base.LinearSlow()
+
+function size(a::SymBitArray)
+    l = length(a.data)
+    n = Int(sqrt(2l+1//4)-1//2)
+    return (n, n)
+end
+
+#getindex(a::SymBitArray, i::Int) = getindex(a.data, i)
+
+function getindex(a::SymBitArray, i::Int, j::Int)
+    ncol = size(a)[1]
+    row = max(i, j)
+    col = min(i, j)
+    c = ncol-col+1
+    ind = Int((ncol*(ncol+1)/2) - (c*(c+1)/2)) + row - col + 1
+
+    return a.data[ind]
+end
+
+#setindex!(a::SymBitArray, v::Bool, i::Int) = setindex!(a.data, v, i)
+
+function setindex!(a::SymBitArray, v::Bool, i::Int, j::Int)
+    ncol = size(a)[1]
+    row = max(i, j)
+    col = min(i, j)
+    c = ncol-col+1
+    ind = Int((ncol*(ncol+1)/2) - (c*(c+1)/2)) + row - col + 1
+
+    a.data[ind] = v
+end
+
+end # module
